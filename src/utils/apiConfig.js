8843add52dac8ee76a9bdd4118070464e25186a2
Jayes23 <jsamuel2580@gmail.com>
2023-08-25 20:08:20 +0100

    Changed api baseUrl

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
-  // baseURL: "http://16.170.228.220/api/v1/",
-  baseURL: 'https://inshopper.io/api/v1/',
+  baseURL: "http://16.170.228.220/api/v1/",
+  // baseURL: 'https://inshopper.io/api/v1/',
 };
 
 export default apiConfig;
