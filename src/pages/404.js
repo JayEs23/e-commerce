b70432bfbe2b4a96ac804183eff3b709cac16d5a
Onyekachi Nwakaihe <kachi@Davids-MacBook-Pro-2.local>
2023-09-23 08:11:44 +0100

    Refactored Products Page

--- src/pages/404.js
+++ src/pages/404.js
@@ -35,9 +35,9 @@ const ErrorPage = ({ statusCode }) => {
   );
 };
 
-ErrorPage.getInitialProps = ({ res, err }) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
-  return { statusCode };
-};
+// ErrorPage.getInitialProps = ({ res, err }) => {
+//   const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+//   return { statusCode };
+// };
 
 export default ErrorPage;
